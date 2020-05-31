565a03e880205c27c46aaafd82d1a00ee1e7670d
上野誉司 <isacatoneu@uenohomareshinoMacBook-puro.local>
2020-05-31 15:48:34 +0900

    add_admin_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'categories/index'
+  end
+  namespace :admin do
+    get 'products/index'
+    get 'products/show'
+    get 'products/new'
+    get 'products/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
